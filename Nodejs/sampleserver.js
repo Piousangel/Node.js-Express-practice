f7fbe5243ebf98bb407495d11c5b3cdfac16b88b
Piousangel <yyh7654@gmail.com>
2021-07-25 23:00:14 +0900

    ejs engine
    exindex.html 연결 by ejs engine
    11: 00

--- Nodejs/sampleserver.js
+++ Nodejs/sampleserver.js
@@ -5,10 +5,18 @@ const server = app.listen(3000, () =>{
     console.log('Start Server : localhost:3000');
 });
 
-app.get('/' , function(req, res){
-    res.send('hi Thankyou')
+app.set('views', __dirname + '/views');
+//app.set('views', path.join(__dirname, 'views'));
+// 화면 engine을 ejs로 설정
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function(req, res){
+    res.render('exindex.html')
 })
 
-app.get('/about', function(req,res){
+app.get('/about', function(req, res){
     res.send('About page')
 })
+
+ 
